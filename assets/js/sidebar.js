98a9389fa5c33b356e0d2a6ea3916d7792fdf75a
long <jinhonara12@naver.com>
2022-12-27 13:44:01 +0900

    사이드바 탑, 다운 버튼 스크립트 삽입

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,9 +1,10 @@
 window.addEventListener('DOMContentLoaded', function () {
     let sideBar = document.getElementById('sidebar');
     let sideOepnBtn = document.getElementById('side_open_btn');
+    let sideTopBtn = document.getElementById('side_top_btn');
+    let sideDownBtn = document.getElementById('side_down_btn');
 
-
-    sideOepnBtn.addEventListener('click', () => {
-        sideBar.classList.toggle('open')
-    })
+    sideOepnBtn.addEventListener('click', () => sideBar.classList.toggle('open'));
+    sideTopBtn.addEventListener('click', () => window.scrollTo(0, 0));
+    sideDownBtn.addEventListener('click', () => window.scrollTo(0, document.body.scrollHeight));
 })
